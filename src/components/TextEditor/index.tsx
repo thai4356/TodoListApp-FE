189e9aed56640bce671727806eb22c8e446aa784
thai4356 <tranquocthai5296@gmail.com>
2025-11-28 23:03:04 +0700

    a

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,15 +1,16 @@
-import Delta from "quill-delta";
-import { useCallback } from "react";
-import { UnprivilegedEditor } from "react-quill";
-import ReactQuillCustom, { QuillProps } from "./ReactQuillCustom";
+import { default as DeltaStatic } from 'quill';
+import { useCallback } from 'react';
+import { UnprivilegedEditor } from 'react-quill';
+import ReactQuillCustom, { QuillProps } from './ReactQuillCustom';
 
-type OnChangeFunc = (
-  value: string,
-  delta: Delta,
-  source: string,
-  editor: UnprivilegedEditor
-) => void;
+type OnChangeFunc = (value: string, delta: DeltaStatic, source: string, editor: UnprivilegedEditor) => void;
 
+/**
+ * Override default onChange behaviour, to only fire once user changes.
+ * Reference: https://github.com/zenoamaro/react-quill/issues/259#issuecomment-343191875
+ * @param props
+ * @constructor
+ */
 interface ITextEditorProps extends QuillProps {
   defaultBehaviour?: boolean;
   bodyHeight?: number;
@@ -17,23 +18,18 @@ interface ITextEditorProps extends QuillProps {
   hiddenBorder?: boolean;
 }
 
-function TextEditor({
-  defaultBehaviour = false,
-  ...restProps
-}: ITextEditorProps): JSX.Element {
-  const handleOnChange = useCallback<OnChangeFunc>(
-    (value, delta, source, editor) => {
-      if (defaultBehaviour) {
-        restProps.onChange?.(value, delta, source, editor);
-        return;
-      }
-
-      if (source === "user") {
-        restProps.onChange?.(value, delta, source, editor);
-      }
-    },
-    [defaultBehaviour, restProps]
-  );
+function TextEditor({ defaultBehaviour = false, ...restProps }: ITextEditorProps): JSX.Element {
+  const handleOnChange = useCallback<OnChangeFunc>((value, delta, source, editor) => {
+    if (defaultBehaviour) {
+      restProps.onChange?.(value, delta, source, editor);
+      return;
+    }
+    // Reference here
+    if (source === 'user') {
+      restProps.onChange?.(value, delta, source, editor);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ReactQuillCustom
